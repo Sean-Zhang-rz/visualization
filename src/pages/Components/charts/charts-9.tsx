326259fe69a4e9e5abf32d809b8f6573a0762ec5
seanMr <770899447@qq.com>
2022-09-25 15:45:12 +0800

    charts 9完成

--- src/pages/Components/charts/charts-9.tsx
+++ src/pages/Components/charts/charts-9.tsx
@@ -1,16 +1,62 @@
 import React, { useEffect, useRef } from 'react';
+import { createEchartsOptions } from '@/shared/create-echarts-options';
+import { px } from '@/shared/px';
 import * as echarts from 'echarts';
 
 export const Chart9 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     // @ts-ignore
-    var myChart = echarts.init(divRef.current);
+    const myChart = echarts.init(divRef.current);
+    myChart.setOption(
+      createEchartsOptions({
+        color: '#F7A110',
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: [0, 18, 28, 38, 48, 58, 68, 78],
+          splitLine: { show: true, lineStyle: { color: '#073E78' } },
+          axisTick: { show: false },
+          axisLine: { show: false },
+        },
+        yAxis: {
+          type: 'value',
+          splitLine: { lineStyle: { color: '#073E78' } },
+          axisLabel: {
+            formatter(val: number) {
+              return val * 100 + '%';
+            },
+          },
+        },
+        series: [
+          {
+            type: 'line',
+            data: [0.19, 0.2, 0.26, 0.35, 0.26, 0.2, 0.08, 0.06],
+            symbol: 'circle',
+            symbolSize: px(12),
+            lineStyle: { width: px(2) },
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: '#F7A110',
+                },
+                {
+                  offset: 1,
+                  color: '#1B1D52',
+                },
+              ]),
+            },
+          },
+        ],
+      })
+    );
   }, []);
 
   return (
     <div className="year-old-range-3">
-      9<div ref={divRef} className="chart"></div>
+      <h3>犯罪年龄趋势图</h3>
+      <div ref={divRef} className="chart"></div>
     </div>
   );
 };
