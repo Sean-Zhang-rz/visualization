77005fab6f55c70b53ddf7ab9299fbe7ba1a599f
seanMr <770899447@qq.com>
2022-09-25 15:05:49 +0800

    提取公共部分

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      components: path.resolve(__dirname, 'src/pages/Components'),
+    },
+  },
   plugins: [reactRefresh()],
 });
